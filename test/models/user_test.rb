6c405eb748771fe70cac95bef45f18d681faece0
MichaelVessia <michaelvessia@mail.adelphi.edu>
2015-08-30 22:28:40 -0400

    Finish login/logout

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -61,4 +61,5 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
   end
+
 end
